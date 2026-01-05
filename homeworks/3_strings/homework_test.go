4c23cb320a2c9cd9f3d0e317d829a72945343fe1
Vladislav Byrgazov <vlados.byrgazov@gmail.com>
2026-01-05 15:46:58 +0500

    homework: implement COW buffer
    Signed-off-by: Vladislav Byrgazov <vlados.byrgazov@gmail.com>

---
+++ homeworks/3_strings/homework_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"reflect"
+	"runtime"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+type COWBuffer struct {
+	data []byte
+	refs *int
+}
+
+func NewCOWBuffer(data []byte) *COWBuffer {
+	ref := 1
+	buff := &COWBuffer{
+		data: data,
+		refs: &ref,
+	}
+	runtime.SetFinalizer(buff, (*COWBuffer).Close)
+	return buff
+}
+
+func (b *COWBuffer) Clone() COWBuffer {
+	*b.refs++
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
+}
+
+func (b *COWBuffer) Close() {
+	runtime.SetFinalizer(b, nil)
+	*b.refs--
+}
+
+func (b *COWBuffer) Update(index int, value byte) bool {
+	if (index >= int(len(b.data))) || (index < 0) {
+		return false
+	}
+
+	if *b.refs > 1 {
+		newData := make([]byte, len(b.data))
+		copy(newData, b.data)
+		newBuff := NewCOWBuffer(newData)
+		*b.refs--
+		b.data = newBuff.data
+		b.refs = newBuff.refs
+	}
+
+	b.data[index] = value
+
+	return true
+}
+
+func (b *COWBuffer) String() string {
+	return unsafe.String(&b.data[0], len(b.data))
+}
+
+func TestCOWBuffer(t *testing.T) {
+	data := []byte{'a', 'b', 'c', 'd'}
+	buffer := NewCOWBuffer(data)
+	defer buffer.Close()
+
+	copy1 := buffer.Clone()
+	copy2 := buffer.Clone()
+
+	assert.Equal(t, unsafe.SliceData(data), unsafe.SliceData(buffer.data))
+	assert.Equal(t, unsafe.SliceData(buffer.data), unsafe.SliceData(copy1.data))
+	assert.Equal(t, unsafe.SliceData(copy1.data), unsafe.SliceData(copy2.data))
+
+	assert.True(t, (*byte)(unsafe.SliceData(data)) == unsafe.StringData(buffer.String()))
+	assert.True(t, (*byte)(unsafe.StringData(buffer.String())) == unsafe.StringData(copy1.String()))
+	assert.True(t, (*byte)(unsafe.StringData(copy1.String())) == unsafe.StringData(copy2.String()))
+
+	assert.True(t, buffer.Update(0, 'g'))
+	assert.False(t, buffer.Update(-1, 'g'))
+	assert.False(t, buffer.Update(4, 'g'))
+
+	assert.True(t, reflect.DeepEqual([]byte{'g', 'b', 'c', 'd'}, buffer.data))
+	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy1.data))
+	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy2.data))
+
+	assert.NotEqual(t, unsafe.SliceData(buffer.data), unsafe.SliceData(copy1.data))
+	assert.Equal(t, unsafe.SliceData(copy1.data), unsafe.SliceData(copy2.data))
+
+	copy1.Close()
+
+	previous := copy2.data
+	copy2.Update(0, 'f')
+	current := copy2.data
+
+	// 1 reference - don't need to copy buffer during update
+	assert.Equal(t, unsafe.SliceData(previous), unsafe.SliceData(current))
+
+	copy2.Close()
+}
+
+func TestCOWBufferFinalizer(t *testing.T) {
+	data := []byte{'a', 'b', 'c', 'd'}
+	buffer := NewCOWBuffer(data)
+	bufferRefs := buffer.refs
+	assert.Equal(t, *bufferRefs, 1)
+	buffer = nil
+	runtime.GC()
+	runtime.Gosched()
+	assert.Equal(t, *bufferRefs, 0)
+}
