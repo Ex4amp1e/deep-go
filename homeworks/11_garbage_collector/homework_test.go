226c69db6c9ea72bb7af86a36c07106ed32ea82e
Vladislav Byrgazov <vlados.byrgazov@gmail.com>
2026-01-11 12:35:02 +0500

    homework: implement tracing dfs func
    Signed-off-by: Vladislav Byrgazov <vlados.byrgazov@gmail.com>

---
+++ homeworks/11_garbage_collector/homework_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func Trace(stacks [][]uintptr) []uintptr {
+	seen := map[uintptr]bool{}
+	result := []uintptr{}
+
+	var dfs func(uintptr)
+	dfs = func(ptr uintptr) {
+		if ptr == 0 || seen[ptr] {
+			return
+		}
+		result = append(result, ptr)
+		seen[ptr] = true
+		next := *(*uintptr)(unsafe.Pointer(ptr))
+		dfs(next)
+	}
+
+	for _, row := range stacks {
+		for _, ptr := range row {
+			dfs(ptr)
+		}
+
+	}
+	return result
+}
+
+func TestTrace(t *testing.T) {
+	var heapObjects = []int{
+		0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+
+	var heapPointer1 *int = &heapObjects[1]
+	var heapPointer2 *int = &heapObjects[2]
+	var heapPointer3 *int = nil
+	var heapPointer4 **int = &heapPointer3
+
+	var stacks = [][]uintptr{
+		{
+			uintptr(unsafe.Pointer(&heapPointer1)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[0])),
+			0x00, 0x00, 0x00, 0x00,
+		},
+		{
+			uintptr(unsafe.Pointer(&heapPointer2)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[1])),
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[2])),
+			uintptr(unsafe.Pointer(&heapPointer4)), 0x00, 0x00, 0x00,
+		},
+		{
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[3])),
+		},
+	}
+
+	pointers := Trace(stacks)
+	expectedPointers := []uintptr{
+		uintptr(unsafe.Pointer(&heapPointer1)),
+		uintptr(unsafe.Pointer(&heapObjects[0])),
+		uintptr(unsafe.Pointer(&heapPointer2)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
+		uintptr(unsafe.Pointer(&heapObjects[2])),
+		uintptr(unsafe.Pointer(&heapPointer4)),
+		uintptr(unsafe.Pointer(&heapPointer3)),
+		uintptr(unsafe.Pointer(&heapObjects[3])),
+	}
+	assert.ElementsMatch(t, expectedPointers, pointers)
+}
